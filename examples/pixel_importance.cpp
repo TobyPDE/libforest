d2b73e3cab4e47fd61a96a3430f5319bc3c9065d
David Stutz <davidstutz@web.de>
2015-03-02 02:11:57 +0100

    Forgot to add pixel importance example.

---
+++ examples/pixel_importance.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include "libforest/libforest.h"
+#include <chrono>
+#include <fstream>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace libf;
+
+/**
+ * Command line tool for visualizing the pixel importance for datasets where
+ * images are given as raw input (e.g. USPS, MNIST).
+ * 
+ * Usage:
+ * $ ./examples/cli_pixel_importance --help
+ * Allowed options:
+ *   --help                                produce help message
+ *   --file-train arg                      path to train DAT file
+ *   --file-test arg                       path to test DAT file
+ *   --output-image arg (=pixel_importance.png)
+ *                                         output image visualizing pixel 
+ *   --output-width arg (=10)              width of output image (assumed to
+ *                                         be square)
+ *                                         importance
+ *   --num-features arg (=10)              number of features to use (set to 
+ *                                         dimensionality of data to learn 
+ *                                         deterministically)
+ *   --use-bootstrap                       use bootstrapping for training
+ *   --num-trees arg (=100)                number of trees in forest
+ *   --max-depth arg (=100)                maximum depth of trees
+ *   --num-threads arg (=1)                number of threads for learning
+ */
+int main(int argc, const char** argv)
+{
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("file-train", boost::program_options::value<std::string>(), "path to train DAT file")
+        ("file-test", boost::program_options::value<std::string>(), "path to test DAT file")
+        ("output-image", boost::program_options::value<std::string>()->default_value("pixel_importance.png"), "output image visualizing pixel importance")
+        ("output-width", boost::program_options::value<int>()->default_value(16), "width of output image (assumed to be square)")
+        ("num-features", boost::program_options::value<int>()->default_value(10), "number of features to use (set to dimensionality of data to learn deterministically)")
+        ("use-bootstrap", "use bootstrapping for training")
+        ("num-trees", boost::program_options::value<int>()->default_value(100), "number of trees in forest")
+        ("max-depth", boost::program_options::value<int>()->default_value(100), "maximum depth of trees")
+        ("num-threads", boost::program_options::value<int>()->default_value(1), "number of threads for learning");
+
+    boost::program_options::positional_options_description positionals;
+    positionals.add("file-train", 1);
+    positionals.add("file-test", 1);
+    
+    boost::program_options::variables_map parameters;
+    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).positional(positionals).run(), parameters);
+    boost::program_options::notify(parameters);
+
+    if (parameters.find("help") != parameters.end())
+    {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+    
+    boost::filesystem::path mnistTrainDat(parameters["file-train"].as<std::string>());
+    if (!boost::filesystem::is_regular_file(mnistTrainDat))
+    {
+        std::cout << "Train DAT file does not exist at the specified location." << std::endl;
+        return 1;
+    }
+    
+    boost::filesystem::path mnistTestDat(parameters["file-test"].as<std::string>());
+    if (!boost::filesystem::is_regular_file(mnistTestDat))
+    {
+        std::cout << "Test DAT file does not exist at the specified location." << std::endl;
+        return 1;
+    }
+    
+    bool useBootstrap = parameters.find("use-bootstrap") != parameters.end();
+    bool computeMDI = parameters.find("compute-mdi") != parameters.end();
+    
+    DataStorage storage;
+    DataStorage storageT;
+    
+    LibforestDataProvider reader;
+    reader.read(mnistTrainDat.string(), &storageT);
+    reader.read(mnistTestDat.string(), &storage);
+    
+    std::cout << "Training Data" << std::endl;
+    storage.dumpInformation();
+    
+    DecisionTreeLearner treeLearner;
+    
+    treeLearner.autoconf(&storage);
+    treeLearner.setUseBootstrap(useBootstrap);
+    treeLearner.setMaxDepth(parameters["max-depth"].as<int>());
+    treeLearner.setNumFeatures(parameters["num-features"].as<int>());
+    
+    RandomForestLearner forestLearner;
+    forestLearner.addCallback(RandomForestLearner::defaultCallback, 1);
+    
+    forestLearner.setTreeLearner(&treeLearner);
+    forestLearner.setNumTrees(parameters["num-trees"].as<int>());
+    forestLearner.setNumThreads(parameters["num-threads"].as<int>());
+    
+    RandomForest* forest = forestLearner.learn(&storage);
+    
+    AccuracyTool accuracyTool;
+    accuracyTool.measureAndPrint(forest, &storageT);
+    
+    ConfusionMatrixTool confusionMatrixTool;
+    confusionMatrixTool.measureAndPrint(forest, &storageT);
+    
+    // USPS images are 16 x 16, MNIST are 28 x 28.
+    PixelImportanceTool piTool;
+    piTool.measureAndPrint(&forestLearner);
+    piTool.measureAndSave(&forestLearner, boost::filesystem::path(parameters["output-image"].as<std::string>()), parameters["output-width"].as<int>());
+    
+    delete forest;
+    return 0;
+}
