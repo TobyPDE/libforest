b5ce4c97fb96f42e3e78634c5626d15d3a409249
davidstutzz <davidstutzz@localhost>
2015-03-03 01:51:49 +0100

    Created Abstract learners for all types in order to derive online learners (in my current analysis, online learner have a bit in commong with offline learner). As result every learner has to derive from two classes (I know, multiple inheritance isnt the best solution, but in this case I hope it works well): From Learner<T> or OnlineLearner<T> and from AbstractXYLearner<LearnerState>. I hope, this way, online algorithms can easily be integrated, especially as it would be possible to learn a model offline using an offline learner, and then update the same model online using an online learner. Cheers!

--- include/libforest/online_learning.h
+++ include/libforest/online_learning.h
@@ -19,15 +19,87 @@ namespace libf {
     class RandomForest;
     
     /**
-     * This is the base class for all online learners
+     * This is the base class for all online learners.
      */
-    template<class T, class S>
-    class OnlineLearner : public AbstractLearner<T, S> {
+    template <class T, class S>
+    class Learner : public AbstractLearner<T, S> {
     public:
+        
         /**
-         * Learns/updates a classifier online
+         * Learns a classifier online (updates a given classifier).
          */
-        virtual T* learn(const DataStorage* storage, T* model = NULL) const = 0;
+        virtual T* learn(const DataStorage* storage, T* model = NULL) = 0;
+
+    };
+    
+    class OnlineDecisionTreeLearner : public AbstractDecisionTreeLearner,
+            public OnlineLearner<DecisionTree, void> {
+    public:
+        OnlineDecisionTreeLearner() : AbstractDecisionTreeLearner, 
+                numThresholds(25),
+                minSplitObjective(0.1f) {};
+        
+        /**
+         * Sets the minimum objective required for a split.
+         */
+        void setMinSplitObjective(float _minSplitObjective)
+        {
+            minSplitObjective = _minSplitObjective;
+        }
+        
+        /**
+         * Returns the minimum objective required for a split.
+         */
+        float getMinSplitObjective()
+        {
+            return minSplitObjective;
+        }
+        
+        /**
+         * Sets the number of thresholds randomly sampled for each node.
+         */
+        void setNumThresholds(int _numThresholds)
+        {
+            numThresholds = _numThresholds;
+        }
+        
+        /**
+         * Returns the number of thresholds randomly sampled for each node.
+         */
+        int getNumThresholds()
+        {
+            return numThresholds;
+        }
+        
+        /**
+         * Configures the learning automatically depending on a certain data
+         * set. 
+         */
+        virtual void autoconf(const DataStorage* storage);
+        
+        /**
+         * Updates the given decision tree on the given data.
+         */
+        virtual DecisionTree* learn(const DataStorage* storage, DecisionTree* tree);
+        
+        /**
+         * Dumps the settings
+         */
+        virtual void dumpSetting(std::ostream & stream = std::cout) const;
+        
+    protected:
+        /**
+         * Minimum objective required for a node to split.
+         */
+        float minSplitObjective;
+        /**
+         * Number of thresholds randomly sampled. Together with the sampled
+         * features these define the tests over which to optimize at
+         * each node in online learning.
+         * 
+         * @see http://lrs.icg.tugraz.at/pubs/saffari_olcv_09.pdf
+         */
+        int numThresholds;
     };
 }
 
