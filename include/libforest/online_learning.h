5df6db93ba82de7c71add10c30be5a52d3e8eee9
David Stutz <davidstutz@web.de>
2015-03-04 18:02:58 -0500

    Working on online decision trees. Seems to be running, but still some things to figure out (60% on USPS, but somehow only 15% on MNISt - there is something worng)!

--- include/libforest/online_learning.h
+++ include/libforest/online_learning.h
@@ -60,9 +60,17 @@ namespace libf {
          */
         int node;
         /**
+         * Samples of node.
+         */
+        int samples;
+        /**
          * Objective of splitted node.
          */
         float objective;
+        /**
+         * Minimum require dobjective.
+         */
+        float minObjective;
         /**
          * Depth of spitted node.
          */
@@ -82,27 +90,127 @@ namespace libf {
         }
     };
     
+    /**
+     * Online decision trees are totally randomized, ie.e. the threshold at each
+     * node is chosen randomly. Therefore, the tree has to know the ranges from
+     * which to pick the thresholds.
+     */
+    class RandomThresholdGenerator {
+    public:
+        
+        /**
+         * Default constructor: use addFeatureRange to add the value range for
+         * an additional feature. Features have to be added in the correct
+         * order!
+         */
+        RandomThresholdGenerator() {};
+        
+        /**
+         * Deduce the feature ranges from the given dataset.
+         */
+        RandomThresholdGenerator(const DataStorage & storage);
+        
+        /**
+         * Adds a feature range. Note that the features have to be added in the 
+         * correct order!
+         */
+        void addFeatureRange(float _min, float _max)
+        {
+            min.push_back(_min);
+            max.push_back(_max);
+        }
+        
+        /**
+         * Adds the same range for num consecutive features.
+         * Note that the features have to be added in the correct order!
+         */
+        void addFeatureRanges(int num, float _min, float _max)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                min.push_back(_min);
+                max.push_back(_max);
+            }
+        }
+        
+        /**
+         * Returns minimum for a specific feature.
+         */
+        float getMin(int feature)
+        {
+            assert(feature >=0 && feature < getSize());
+            return min[feature];
+        }
+        
+        /**
+         * Returns maximum for a specific feature.
+         */
+        float getMax(int feature)
+        {
+            assert(feature >= 0 && feature < getSize());
+            return max[feature];
+        }
+        
+        /**
+         * Samples a value uniformly for the given feature.
+         */
+        float sample(int feature);
+        
+        /**
+         * Returns the size of the generator (number of features).
+         */
+        int getSize()
+        {
+            return min.size();
+        }
+        
+    protected:
+        /**
+         * Minimum value for each feature.
+         */
+        std::vector<float> min;
+        /**
+         * Maximum value for each feature.
+         */
+        std::vector<float> max;
+    };
+    
+    /**
+     * Learn a decision tree online, either by passing a single sample at a time
+     * or doing online batch learning.
+     */
     class OnlineDecisionTreeLearner : public AbstractDecisionTreeLearner<OnlineDecisionTreeLearnerState>,
             public OnlineLearner<DecisionTree> {
     public:
         OnlineDecisionTreeLearner() : AbstractDecisionTreeLearner(), 
-                numThresholds(25),
-                minSplitObjective(0.1f) {};
+                numThresholds(2*numFeatures),
+                minSplitObjective(1.f)
+        {
+            // Overwrite min split examples.
+            minSplitExamples = 30;
+            minChildSplitExamples = 15;
+        }
         
         /**
          * The default callback for this learner.
          */
         static int defaultCallback(DecisionTree* tree, OnlineDecisionTreeLearnerState* state);
-                
+        
+        /**
+         * Verbose callback for this learner.
+         */
+        static int verboseCallback(DecisionTree* tree, OnlineDecisionTreeLearnerState* state);
+        
         /**
          * Actions for the callback function.
          */
         const static int ACTION_START_TREE = 1;
         const static int ACTION_UPDATE_TREE = 2;
         const static int ACTION_INIT_NODE = 3;
-        const static int ACTION_UPDATE_NODE = 4;
-        const static int ACTION_SPLIT_NODE = 5;
-                
+        const static int ACTION_NOT_SPLITTING_NODE = 4;
+        const static int ACTION_NOT_SPLITTING_OBJECTIVE_NODE = 5;
+        const static int ACTION_SPLIT_NODE = 6;
+        
         /**
          * Sets the minimum objective required for a split.
          */
@@ -138,16 +246,32 @@ namespace libf {
         }
         
         /**
+         * Sets the threshold generator to use.
+         */
+        void setThresholdGenerator(RandomThresholdGenerator _thresholdGenerator)
+        {
+            thresholdGenerator = _thresholdGenerator;
+        }
+        
+        /**
+         * Returns the used threshold generator.
+         */
+        RandomThresholdGenerator & getThresholdGenerator()
+        {
+            return thresholdGenerator;
+        }
+        
+        /**
          * Updates the given decision tree on the given data.
          */
-        virtual DecisionTree* learn(const DataStorage* storage, DecisionTree* tree);
+        virtual DecisionTree* learn(const DataStorage* storage, DecisionTree* tree = 0);
         
     protected:
         /**
          * For all splits, update left and right child statistics.
          */
-        void updateSplitStatistics(std::vector<EfficientEntropyHistogram> leftChildStatistics, 
-                std::vector<EfficientEntropyHistogram> rightChildStatistics, 
+        void updateSplitStatistics(std::vector<EfficientEntropyHistogram> & leftChildStatistics, 
+                std::vector<EfficientEntropyHistogram> & rightChildStatistics, 
                 const std::vector<int> & features,
                 const std::vector< std::vector<float> > & thresholds, 
                 const std::pair<DataPoint*, int> & x);
@@ -164,6 +288,10 @@ namespace libf {
          * @see http://lrs.icg.tugraz.at/pubs/saffari_olcv_09.pdf
          */
         int numThresholds;
+        /**
+         * The generator to sample random thresholds.
+         */
+        RandomThresholdGenerator thresholdGenerator;
     };
 }
 
