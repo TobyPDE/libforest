5e0815dc8d88d06fc77abb1ecb01ce2d9c8a6dcc
David Stutz <davidstutz@web.de>
2015-03-11 17:51:31 -0400

    Forget to add unsupervised files. The described segmentation fault might be because I added the base class "Tree" from which DensityTree and DecisioNTree inherit, but I really could not figure it out.

---
+++ include/libforest/estimators.h
@@ -0,0 +1,211 @@
+#ifndef LIBF_ESTIMATORS_H
+#define LIBF_ESTIMATORS_H
+
+/**
+ * This file contains the data structures for the classifiers. There are 
+ * basically two kinds ot classifiers:
+ * 1. Decision trees
+ * 2. Random forests
+ */
+
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+
+// For gaussian sampling
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/variate_generator.hpp>
+
+#include "util.h"
+#include "data.h"
+#include "classifiers.h"
+
+namespace libf {
+    /**
+     * Forward declarations to speed up compiling
+     */
+    class DataStorage;
+    class DataPoint;
+    
+    class Estimator {
+    public:
+        /**
+         * Estimate the probability of a datapoint.
+         */
+        virtual float estimate(const DataPoint* x) = 0;
+    };
+    
+    /**
+     * A simple Gaussian distribution represented by mean and covariance matrix.
+     */
+    class Gaussian {
+    public:
+        /**
+         * Default Gaussian with zero mean and identity covariance.
+         */
+        Gaussian() :
+                cachedInverse(false),
+                cachedDeterminant(false),
+                randN(rng, norm) {};
+        
+        /**
+         * Gaussian with given mean and covariance.
+         */
+        Gaussian(Eigen::VectorXf _mean, Eigen::MatrixXf _covariance);
+        
+        /**
+         * Gaussian with given mean and covariance and cached determinant.
+         */
+        Gaussian(Eigen::VectorXf _mean, Eigen::MatrixXf _covariance, float _covarianceDeterminant);
+                
+        /**
+         * Destructor.
+         */
+        ~Gaussian() {};
+        
+        Gaussian operator=(const Gaussian & other)
+        {
+            mean = Eigen::VectorXf(other.mean);
+            covariance = Eigen::MatrixXf(other.covariance);
+
+            transform = Eigen::MatrixXf(other.transform);
+            
+            cachedInverse = false;
+            cachedDeterminant = false;
+            
+            return *this;
+        }
+        
+        /**
+         * Get probability of the given data point.
+         */
+        float evaluate(const DataPoint* x);
+        
+        /**
+         * Sample a point from the Gaussian.
+         */
+        DataPoint* sample();
+        
+        /**
+         * Sets the mean.
+         */
+        void setMean(Eigen::VectorXf _mean)
+        {
+            mean = Eigen::VectorXf(_mean);
+        }
+        
+        /**
+         * Returns the mean.
+         */
+        Eigen::VectorXf & getMean()
+        {
+            return mean;
+        }
+        
+        /**
+         * Sets the covariance matrix.
+         */
+        void setCovariance(Eigen::MatrixXf _covariance);
+        
+        /**
+         * Returns the covariance matrix.
+         */
+        Eigen::MatrixXf getCovariance()
+        {
+            return covariance;
+        }
+        
+    private:
+        Eigen::VectorXf asEigenVector(const DataPoint* x);
+        DataPoint* asDataPoint(const Eigen::VectorXf & y);
+        
+        /**
+         * Mean of Gaussian.
+         */
+        Eigen::VectorXf mean;
+        /**
+         * Covariance of Gaussian.
+         */
+        Eigen::MatrixXf covariance;
+        /**
+         * Inverse covariance can be cached.
+         */
+        bool cachedInverse = false;
+        /**
+         * Covariance determinant can be cached.
+         */
+        bool cachedDeterminant = false;
+        /**
+         * Cached covariance inverse.
+         */
+        Eigen::MatrixXf covarianceInverse;
+        /**
+         * Cached determinant.
+         */
+        float covarianceDeterminant;
+        /**
+         * Uniform pseudo random generator.
+         */
+        boost::mt19937 rng;
+        /**
+         * Scalar Gaussian distribution.
+         */
+        boost::normal_distribution<float> norm;
+        /**
+         * Zero mean and unit variance Gaussian distribution.
+         */
+        boost::variate_generator<boost::mt19937&, boost::normal_distribution<float> > randN;
+        /**
+         * Eigenvector and eigenvalue transformation for sampling.
+         */
+        Eigen::MatrixXf transform;
+    };
+    
+    /**
+     * Density decision tree for unsupervised learning.
+     */
+    class DensityTree : public Tree, public Estimator {
+    public:
+        /**
+         * Creates an empty density tree.
+         */
+        DensityTree() : Tree() {};
+        
+        /**
+         * Destructor.
+         */
+        ~DensityTree() {};
+        
+        /**
+         * Get the Gaussian of a specific leaf.
+         */
+        Gaussian & getGaussian(const int node)
+        {
+            return gaussians[node];
+        }
+        
+        /**
+         * Estimate the probability of a datapoint.
+         */
+        virtual float estimate(const DataPoint* x);
+        
+        /**
+         * Reads the classifier from a stream
+         */
+        virtual void read(std::istream & stream);
+        
+        /**
+         * Writes the classifier to a stream
+         */
+        virtual void write(std::ostream & stream) const;
+        
+    private:
+        /**
+         * The Gaussians at the leafs.
+         */
+        std::vector<Gaussian> gaussians;
+        
+    };
+}
+#endif
