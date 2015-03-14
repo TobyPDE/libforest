69da96278cbb3b771b1ca2a879f7df60d89f7402
David Stutz <davidstutz@web.de>
2015-03-14 11:10:21 -0400

    Minor fix for finding gtest.

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -6,8 +6,8 @@
 #  GTest_FOUND       - True if GTest found.
 
 find_path (GTest_INCLUDE_DIR gtest/
-    PATHS ${GFLAGS_ROOT_DIR} ~/gtest-1.7.0/include)
+    PATHS ${GTest_ROOT_DIR} ~/gtest-1.7.0/include)
 find_library (GTest_LIBRARIES NAMES fftw3f
-    PATHS ${GFLAGS_ROOT_DIR} ~/gtest-1.7.0/include)
+    PATHS ${GTest_ROOT_DIR} ~/gtest-1.7.0/include)
 
 mark_as_advanced (GTest_INCLUDE_DIR GTest_LIBRARIES)
